aff4e18e76f1ad99e10f4b733b54c3b3a6052d20
den4ick800 <71667907+den4ick800@users.noreply.github.com>
2020-11-09 19:07:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+//Прыгунов Данил вариант 16 , побитовый сдвиг вправо на 2 бита
+using namespace std;
+int main(int argc, const char* argv[]) {
+    if (argc != 3) {
+        cerr << "Error: Use three parameters\n"; return 5;
+    }
+    const string mode(argv[1]); // Режим работы
+    const string file_name(argv[2]); // Имя файла
+     if (mode == "encryption") {
+    // Режим шифрование
+    char str;
+    cout<<"Write text(length-1)\n";
+    cin>>str;
+    int password;
+    cout<<"Write password"<<endl<<"Password-";
+    cin>>password;
+    int32_t h=str;
+    srand(password);
+    h=h^rand();
+    h=(h >> 2) | (h << (32 - 2));
+    ofstream file;
+    //помещаем значение в файл
+    file.open(file_name, ios::binary);
+    file << h;
+    file.close();
+}
+else if (mode == "decryption") {
+// Режим расшифрования
+//Достаем зашифрованные данные из файла
+int32_t n;
+ifstream file;
+file.open(file_name, ios::binary);
+file>>n;
+int password;
+cout<<"Write password"<<endl<<"Password-";
+cin>>password;
+srand(password);
+n=(n << 2) | (n >> (32 - 2));
+n=n^rand();
+cout<<n<<endl;
+char b =n;
+cout<<b;
+
+}
+return 0; }
